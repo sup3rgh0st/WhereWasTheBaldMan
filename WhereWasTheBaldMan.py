48fcd84be372424738fdf57aa4c0031a6942c44f
sup3rgh0st <sup3rgh0st@users.noreply.github.com>
2020-01-19 15:18:38 -0600

    Retry Loop
    Added a retry loop in case youtube-dl fails to download a vod

--- WhereWasTheBaldMan.py
+++ WhereWasTheBaldMan.py
@@ -13,10 +13,11 @@ from twitchdl import commands
 from twitchdl import twitch
 
 pytesseract.pytesseract.tesseract_cmd = r'D:\Program Files (D)\Tesseract-OCR\tesseract.exe'
+max_download_attempts = 10
+channel_name = "aurateur"
 
 print("\nWhere Was The Bald Man? - sup3rgh0st\n")
 
-channel_name = "aurateur"
 print("Looking up the latest VOD on the channel '"+channel_name+"'...")
 
 # Get the URL of the latest VOD...
@@ -52,17 +53,27 @@ except:
     exit()
 
 print("Found the latest VOD! Downloading...")
-command = "youtube-dl " + vod_url + " -o " + output_video_name + " -c -R 10"
-print("  Starting SubProcess using the command '" + command + "'")
-p = subprocess.Popen(command, shell=True)
-p.wait()
-print ("Done downloading the VOD!")
 
-video = cv2.VideoCapture(output_video_name)
-if (video.isOpened() == False):
-    print("Unable to open the downloaded video '" + output_video_name + "'")
-    output_file.close()
-    exit()
+video = cv2.VideoCapture()
+attempt_count = 1
+while True:
+    command = "youtube-dl " + vod_url + " -o " + output_video_name + " -c -R 10"
+    print("  Starting SubProcess using the command '" + command + "'")
+    p = subprocess.Popen(command, shell=True)
+    p.wait()
+    print ("Done downloading the VOD!")
+
+    video = cv2.VideoCapture(output_video_name)
+    if (video.isOpened() == False):
+        print("Unable to open the downloaded video '" + output_video_name + "'")
+        if attempt_count <= max_download_attempts:
+            print("Retrying... Attempt " + str(attempt_count))
+            continue
+        else:
+            print("Hit Max Retry Count. Quitting.")
+            output_file.close()
+            exit()
+    break
 
 video_fps = video.get(cv2.CAP_PROP_FPS)
 video_framecount = video.get(cv2.CAP_PROP_FRAME_COUNT)
